555d269305eadb77fd5fe416dda98ac596a49d33
kitsune-guuji <103903444+kitsune-guuji@users.noreply.github.com>
2022-06-06 21:45:54 +0400

    fields don't need to bo public sicne we have new()

--- parser/src/model.rs
+++ parser/src/model.rs
@@ -2,8 +2,8 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct Guide {
-    pub id: String,
-    pub html: String,
+    id: String,
+    html: String,
 }
 
 impl Guide {
